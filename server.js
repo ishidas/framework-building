f25c0b062065cb712379e89717b87b71860869c1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-12 15:33:56 -0800

    deleting files in log directory when I request delete via superagent

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-// var fs = require('fs');
+var fs = require('fs');
 var Router = require('./router.js');
-
+// var url = require('url');
 var Articles = new Router();
+// var urlNums;
 
 Articles.get('/articles', (req, res) => {
   res.writeHead(200, {'content-type': 'text/html'});
@@ -11,17 +12,34 @@ Articles.get('/articles', (req, res) => {
 });
 
 Articles.post('/articles', (req, res) =>{
+  // console.dir('Here is current : ' + url.pathname);
+  var date = new Date();
+  var time = date.getTime();
   req.on('data', (data) =>{
-    res.writeHead(200, {'content-type': 'text/html'});
-    console.log('my post: ' + data);
-    res.end();
+    var incoming = data.toString();
+    console.log('stringified data : ' + incoming );
+    fs.writeFile( __dirname + '/log/' + time + '.txt', date + ' ' + time, (err)=>{
+      console.log(err);
+      res.writeHead(200, {'content-type': 'text/html'});
+      console.log('my post: ' + data);
+      res.end();
+    });
   });
 });
 
-Articles.delete('/articles', (req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'});
-  console.log('delete is working');
-  res.end();
+Articles.delete('/articles' , (req, res) => {
+  fs.readdir( __dirname + '/log', (err, files)=>{
+    try {
+      res.writeHead(200, {'content-type': 'text/html'});
+      files.forEach((file)=>{
+        fs.unlink( __dirname + '/log/' + file);
+      });
+      console.log('delete is working');
+      res.end();
+    } catch (err){
+      console.log('This is file deleting err : ' + err);
+    }
+  });
 });
 
 module.exports = http.createServer(Articles.route()).listen(3000, function(){
