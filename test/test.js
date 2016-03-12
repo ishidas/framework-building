7ae23db22b9edb9ff0cdfec0806b03d5fa9b282b
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-12 13:58:45 -0800

    test for delete added

--- test/test.js
+++ test/test.js
@@ -9,9 +9,30 @@ var server = require( __dirname + '/../server.js');
 
 describe('Testing if the server is routing req', function(){
   after(()=> server.close());
-  it('should write head when req come through', function(done){
+  it('should write head when req come through /articles via get route', (done) =>{
     request('localhost:3000')
     .get('/articles')
+    .end((err, res)=>{
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('should write head when req come through /articles via post route', (done) =>{
+    request('localhost:3000')
+    .post('/articles')
+    .send({})
+    .end((err, res)=>{
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+  it('should write head when req come through /articles via delete route', (done)=>{
+    console.log('is this test working??');
+    request('localhost:3000')
+    .delete('/articles')
+    .send({'content-type': 'text/html'})
     .end((err, res)=>{
       expect(err).to.be.null;
       expect(res).to.have.status(200);
