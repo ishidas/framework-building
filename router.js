7140021406300df35388372594355f37b3ba78a1
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-13 22:58:50 -0700

    fixing route so it will take any route name where :id

--- router.js
+++ router.js
@@ -26,7 +26,19 @@ Router.prototype.delete = function (route, cb) {
 
 Router.prototype.route = function () {
   return (req, res) => {
+    var incomingUrl = req.url.split('/');
     var routeFunction = this.routes[req.method][req.url];
+    if(incomingUrl[2] !== ':id' ){
+      req.url = '/' + incomingUrl[1] + '/' + incomingUrl[2];
+    }
+
+    console.log(incomingUrl);
+    console.log(req.method);
     routeFunction(req, res);
   };
 };
+
+// Router.prototype.getUrl = function(){
+//   console.log(this.routes[req.method][req.url]);
+//   return this.routes[req.method][req.url];
+// };
--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 var http = require('http');
 var fs = require('fs');
 var Router = require('./router.js');
-// var url = require('url');
-var Articles = new Router();
-// var urlNums;
 
-Articles.get('/articles', (req, res) => {
+var Articles = new Router();
+
+
+
+Articles.get('/articles/:id', (req, res) => {
   res.writeHead(200, {'content-type': 'text/html'});
-  console.log('I am here at get.');
+  res.write('Hello World');
+  // console.log('I am here at get.');
   res.end();
 });
 
 Articles.post('/articles', (req, res) =>{
-  // console.dir('Here is current : ' + url.pathname);
   var date = new Date();
   var time = date.getTime();
   req.on('data', (data) =>{
-    var incoming = data.toString();
-    console.log('stringified data : ' + incoming );
     fs.writeFile( __dirname + '/log/' + time + '.txt', date + ' ' + time, (err)=>{
       console.log(err);
       res.writeHead(200, {'content-type': 'text/html'});
       console.log('my post: ' + data);
+      res.write('Hello');
       res.end();
     });
   });
 });
 
-Articles.delete('/articles' , (req, res) => {
+Articles.delete('/articles', (req, res) => {
   fs.readdir( __dirname + '/log', (err, files)=>{
     try {
       res.writeHead(200, {'content-type': 'text/html'});
       files.forEach((file)=>{
         fs.unlink( __dirname + '/log/' + file);
       });
-      console.log('delete is working');
+      // console.log('delete is working');
       res.end();
     } catch (err){
-      console.log('This is file deleting err : ' + err);
+      // console.log('This is file deleting err : ' + err);
     }
   });
 });
